1d7e2ff4ae874d621a9c4657df482e9943f4b802
Maxime Haineault <max@motion-m.ca>
2012-07-17 13:23:33 -0400

    Fixed admin for modeltranslation

--- slider/admin.py
+++ slider/admin.py
@@ -5,13 +5,23 @@ from slider.utils import AdminThumbnailMixin, file_cleanup
 from slider.conf import settings
 from slider.models import *
 
+try:
+    from grappellifit.admin import TranslationAdmin
+    BaseAdmin = TranslationAdmin
+except:
+    try:
+        from modeltranslation.admin import TranslationAdmin
+        BaseAdmin = TranslationAdmin
+    except:
+        BaseAdmin = admin.ModelAdmin
+
 
 class SliderAdmin(admin.ModelAdmin):
     list_display = ('slug',)
 admin.site.register(Slider, SliderAdmin)
 
 
-class SliderImageAdmin(admin.ModelAdmin, AdminThumbnailMixin):
+class SliderImageAdmin(BaseAdmin, AdminThumbnailMixin):
     list_display = ('thumbnail', 'title', 'slider', 'position', 'crop', 'sharpen', 'upscale', 'is_visible',)
     list_editable = ('title', 'position', 'is_visible',)
     list_filter = ('slider', 'is_visible', 'sharpen', 'upscale')
